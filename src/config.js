2d540eae9efb68119662c31dc4ea6987a6f42317
Local-Bias <echo07200503@gmail.com>
2020-11-16 04:20:09 +0900

    fix indent

--- src/config.js
+++ src/config.js
@@ -2,46 +2,46 @@
 import {plugin} from './kintone.client';
 
 (PLUGIN_ID => {
-	'use strict';
+  'use strict';
 
-	const allRecords = document.getElementById('allRecords');
-	const allFields = document.getElementById('allFields');
-	const union = document.getElementById('union');
+  const allRecords = document.getElementById('allRecords');
+  const allFields = document.getElementById('allFields');
+  const union = document.getElementById('union');
 
-	onload();
+  onload();
 
-	const submitButton = document.getElementById('lb-submitButton');
+  const submitButton = document.getElementById('lb-submitButton');
 
-	if (submitButton) {
-		submitButton.addEventListener('click', clicked => {
+  if (submitButton) {
+    submitButton.addEventListener('click', clicked => {
 
-			const config = plugin.getConfig({'id': PLUGIN_ID});
+      const config = plugin.getConfig({'id': PLUGIN_ID});
 
-			config.allRecords = allRecords.checked;
-			config.allFields = allFields.checked;
-			config.union = union.checked;
+      config.allRecords = allRecords.checked;
+      config.allFields = allFields.checked;
+      config.union = union.checked;
 
-			plugin.setConfig({'config': config});
-		});
-	}
+      plugin.setConfig({'config': config});
+    });
+  }
 
-	// キャンセルボタンクリック時の処理
-	const cancelButton = document.getElementById('lb-cancelButton');
-	if (cancelButton) {
-		cancelButton.addEventListener('click', () => history.back());
-	}
+  // キャンセルボタンクリック時の処理
+  const cancelButton = document.getElementById('lb-cancelButton');
+  if (cancelButton) {
+    cancelButton.addEventListener('click', () => history.back());
+  }
 
-	function onload() {
-		const config = plugin.getConfig({'id': PLUGIN_ID});
+  function onload() {
+    const config = plugin.getConfig({'id': PLUGIN_ID});
 
-		if (typeof config.allRecords !== 'undefined') {
-			allRecords.checked = config.allRecords;
-		}
-		if (typeof config.allFields !== 'undefined') {
-			allFields.checked = config.allFields;
-		}
-		if (typeof config.union !== 'undefined') {
-			union.checked = config.union;
-		}
-	}
+    if (typeof config.allRecords !== 'undefined') {
+      allRecords.checked = config.allRecords;
+    }
+    if (typeof config.allFields !== 'undefined') {
+      allFields.checked = config.allFields;
+    }
+    if (typeof config.union !== 'undefined') {
+      union.checked = config.union;
+    }
+  }
 })(kintone.$PLUGIN_ID);
