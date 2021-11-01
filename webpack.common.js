44eba613731faa9735d057b4ec8245b0867abc70
Ribbit <echo07200503@gmail.com>
2021-11-01 12:02:29 +0900

    リソースのCDN化

--- webpack.common.js
+++ webpack.common.js
@@ -1,18 +1,15 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
   entry: {
-    desktop: "./src/customize.js",
-    config: "./src/config.js",
+    desktop: './src/customize.js',
+    config: './src/config.js',
   },
-  output: {
-    path: path.resolve(__dirname, "plugin", "js"),
-    filename: "[name].js",
-  },
+
   resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx", "json"],
+    extensions: ['.ts', '.tsx', '.js', '.jsx', 'json'],
     alias: {
-      "@common": path.resolve(__dirname, "src/common"),
+      '@common': path.resolve(__dirname, 'src/common'),
     },
   },
   module: {
@@ -21,7 +18,7 @@ module.exports = {
         test: /.tsx?$/,
         exclude: /node_modules/,
         use: {
-          loader: "ts-loader",
+          loader: 'ts-loader',
           options: {
             transpileOnly: true,
           },
