dae5abce64a09702638ed8b6df67430d30ded154
Ribbit <echo07200503@gmail.com>
2023-09-19 08:49:18 +0900

    ♻ 記法を最新化

--- src/desktop/embedding-xlsx-button/index.ts
+++ src/desktop/embedding-xlsx-button/index.ts
@@ -1,42 +1,41 @@
-import { KintoneEventListener, restoreStorage } from '@konomi-app/kintone-utilities';
+import { restoreStorage } from '@konomi-app/kintone-utilities';
 import { getButton } from './button-creation';
 import { download } from './conversion';
 import { getHeaderSpace } from '@lb-ribbit/kintone-xapp';
 import { createConfig } from '@/common/plugin';
+import { listener } from '@/common/listener';
 
 const BUTTON_ID = 'ribbit-plugin-xlsx';
 
-export default (listener: KintoneEventListener) => {
-  listener.add(['app.record.index.show'], async (event, otherProps) => {
-    const { pluginId } = otherProps ?? {};
-    if (!pluginId) {
-      return event;
-    }
-    // 既に設置済みの場合は処理しません
-    if (document.querySelector(`#${BUTTON_ID}`)) {
-      return event;
-    }
-
-    const storage = restoreStorage(pluginId) ?? createConfig();
-
-    const button = getButton(BUTTON_ID);
-    const headerMenuSpace = getHeaderSpace(event.type);
-    if (headerMenuSpace) {
-      headerMenuSpace.append(button);
-    }
-
-    // クリック時のイベントを作成します
-    button.onclick = async () => {
-      button.disabled = true;
-      try {
-        await download(event, storage);
-      } catch (error) {
-        console.error(error);
-      } finally {
-        button.disabled = false;
-      }
-    };
-
+listener.add(['app.record.index.show'], async (event, otherProps) => {
+  const { pluginId } = otherProps ?? {};
+  if (!pluginId) {
     return event;
-  });
-};
+  }
+  // 既に設置済みの場合は処理しません
+  if (document.querySelector(`#${BUTTON_ID}`)) {
+    return event;
+  }
+
+  const storage = restoreStorage(pluginId) ?? createConfig();
+
+  const button = getButton(BUTTON_ID);
+  const headerMenuSpace = getHeaderSpace(event.type);
+  if (headerMenuSpace) {
+    headerMenuSpace.append(button);
+  }
+
+  // クリック時のイベントを作成します
+  button.onclick = async () => {
+    button.disabled = true;
+    try {
+      await download(event, storage);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      button.disabled = false;
+    }
+  };
+
+  return event;
+});
--- src/desktop/index.ts
+++ src/desktop/index.ts
@@ -1,22 +1,4 @@
 /* このファイルはビルドの基点になります。 ファイル名、ディレクトリを変更すると、ビルドが正常に動作しない可能性があります */
 
-import { PLUGIN_NAME } from '@/common/static';
-import event from './embedding-xlsx-button';
-import { KintoneEventListener } from '@konomi-app/kintone-utilities';
-import { pushPluginName } from '@/common/local-storage';
-
-((pluginId) => {
-  try {
-    pushPluginName();
-  } catch (error) {}
-  const listener = new KintoneEventListener({
-    pluginId,
-    errorHandler: (error, props) => {
-      const { event } = props;
-      event.error = `プラグイン「${PLUGIN_NAME}」の処理内でエラーが発生しました。`;
-      console.error('エラー', error);
-    },
-    logDisabled: process.env.NODE_ENV === 'production',
-  });
-  event(listener);
-})(kintone.$PLUGIN_ID);
+import '@/common/global';
+import './embedding-xlsx-button';
