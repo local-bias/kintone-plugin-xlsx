04cfa9e897bb306824a7ae46070a9a325589da16
Ribbit <echo07200503@gmail.com>
2025-03-30 15:50:28 +0900

    ✨ Appコンポーネントを関数形式に変更し、コードの可読性を向上

--- src/config/app.tsx
+++ src/config/app.tsx
@@ -1,39 +1,39 @@
-import { SnackbarProvider } from 'notistack';
-import React, { FC, Suspense } from 'react';
-import { RecoilRoot } from 'recoil';
 import { PluginErrorBoundary } from '@/common/components/functional/error-boundary';
 import { URL_BANNER, URL_PROMOTION } from '@/common/static';
-import Footer from './components/model/footer';
-import Form from './components/model/form';
 import { PluginBanner, PluginContent, PluginLayout } from '@konomi-app/kintone-utilities-react';
 import { LoaderWithLabel } from '@konomi-app/ui-react';
+import { SnackbarProvider } from 'notistack';
+import React, { Suspense } from 'react';
+import { RecoilRoot } from 'recoil';
+import Footer from './components/model/footer';
+import Form from './components/model/form';
 
-const Component: FC = () => (
-  <Suspense fallback={<LoaderWithLabel label='画面の描画を待機しています' />}>
-    <RecoilRoot>
-      <PluginErrorBoundary>
-        <SnackbarProvider maxSnack={1}>
-          <Suspense fallback={<LoaderWithLabel label='設定情報を取得しています' />}>
-            <PluginLayout singleCondition>
-              <PluginContent>
-                <PluginErrorBoundary>
-                  <Form />
-                </PluginErrorBoundary>
-              </PluginContent>
-              <PluginBanner url={URL_BANNER} />
-              <Footer />
-            </PluginLayout>
-          </Suspense>
-        </SnackbarProvider>
-      </PluginErrorBoundary>
-    </RecoilRoot>
-    <iframe
-      title='promotion'
-      loading='lazy'
-      src={URL_PROMOTION}
-      style={{ border: '0', width: '100%', height: '64px' }}
-    />
-  </Suspense>
-);
-
-export default Component;
+export default function App() {
+  return (
+    <Suspense fallback={<LoaderWithLabel label='画面の描画を待機しています' />}>
+      <RecoilRoot>
+        <PluginErrorBoundary>
+          <SnackbarProvider maxSnack={1}>
+            <Suspense fallback={<LoaderWithLabel label='設定情報を取得しています' />}>
+              <PluginLayout singleCondition>
+                <PluginContent>
+                  <PluginErrorBoundary>
+                    <Form />
+                  </PluginErrorBoundary>
+                </PluginContent>
+                <PluginBanner url={URL_BANNER} />
+                <Footer />
+              </PluginLayout>
+            </Suspense>
+          </SnackbarProvider>
+        </PluginErrorBoundary>
+      </RecoilRoot>
+      <iframe
+        title='promotion'
+        loading='lazy'
+        src={URL_PROMOTION}
+        style={{ border: '0', width: '100%', height: '64px' }}
+      />
+    </Suspense>
+  );
+}
