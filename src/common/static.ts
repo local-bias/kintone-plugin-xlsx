2272a1e16aee096cfa1980929b756eebfc63ac01
Ribbit <echo07200503@gmail.com>
2025-03-30 15:49:08 +0900

    🔧 設定ファイルからプラグイン名とホームページURLを取得するように修正

--- src/common/static.ts
+++ src/common/static.ts
@@ -1,10 +1,14 @@
-import manifest from '../../plugin/manifest.json';
+import config from '@/../plugin.config.mjs';
 
-export const PLUGIN_NAME = manifest.name.ja;
+const {
+  manifest: { base: baseManifest },
+} = config;
+
+export const PLUGIN_NAME = baseManifest.name.ja;
 
 export const LOCAL_STORAGE_KEY = 'ribbit-kintone-plugin';
 
-export const URL_HOMEPAGE = manifest.homepage_url.ja;
+export const URL_HOMEPAGE = baseManifest.homepage_url.ja;
 export const URL_INQUIRY = 'https://form.konomi.app';
 export const URL_PROMOTION = 'https://promotion.konomi.app/kintone-plugin';
 export const URL_BANNER = 'https://promotion.konomi.app/kintone-plugin/sidebar';
