e6aa38822f7051d92eb527076babdb511f233f57
Ribbit <echo07200503@gmail.com>
2025-04-12 21:31:57 +0900

    ♻ recoil → jotai

--- src/config/components/model/footer/index.tsx
+++ src/config/components/model/footer/index.tsx
@@ -1,26 +1,41 @@
+import { loadingAtom, loadingEndAtom, loadingStartAtom } from '@/common/global-state';
 import styled from '@emotion/styled';
 import { storeStorage } from '@konomi-app/kintone-utilities';
+import { PluginFooter } from '@konomi-app/kintone-utilities-react';
 import SaveIcon from '@mui/icons-material/Save';
 import SettingsBackupRestoreIcon from '@mui/icons-material/SettingsBackupRestore';
 import { Button, CircularProgress } from '@mui/material';
-import { useSnackbar } from 'notistack';
-import React, { FC, FCX, useCallback } from 'react';
-import { useRecoilCallback, useRecoilValue } from 'recoil';
-
-import { loadingState, storageState } from '../../../states/plugin';
-
+import { atom, useAtomValue, useSetAtom } from 'jotai';
+import { enqueueSnackbar } from 'notistack';
+import React, { FC, useCallback } from 'react';
+import { pluginConfigAtom } from '../../../states/plugin';
 import ExportButton from './export-button';
 import ImportButton from './import-button';
 import ResetButton from './reset-button';
-import { PluginFooter } from '@konomi-app/kintone-utilities-react';
 
-type Props = {
-  onSaveButtonClick: () => void;
-  onBackButtonClick: () => void;
-};
+const handlePluginConfigSaveAtom = atom(null, (get, set) => {
+  set(loadingStartAtom);
+  try {
+    const storage = get(pluginConfigAtom);
 
-const Component: FCX<Props> = ({ className, onSaveButtonClick, onBackButtonClick }) => {
-  const loading = useRecoilValue(loadingState);
+    storeStorage(storage!, () => true);
+    enqueueSnackbar('設定を保存しました', {
+      variant: 'success',
+      action: (
+        <Button color='inherit' size='small' variant='outlined' onClick={() => history.back()}>
+          プラグイン一覧に戻る
+        </Button>
+      ),
+    });
+  } finally {
+    set(loadingEndAtom);
+  }
+});
+
+const Component: FC<{ className?: string }> = ({ className }) => {
+  const loading = useAtomValue(loadingAtom);
+  const onBackButtonClick = useCallback(() => history.back(), []);
+  const onSaveButtonClick = useSetAtom(handlePluginConfigSaveAtom);
 
   return (
     <PluginFooter {...{ className }}>
@@ -61,35 +76,4 @@ const StyledComponent = styled(Component)`
   }
 `;
 
-const Container: FC = () => {
-  const { enqueueSnackbar } = useSnackbar();
-
-  const onBackButtonClick = useCallback(() => history.back(), []);
-
-  const onSaveButtonClick = useRecoilCallback(
-    ({ set, snapshot }) =>
-      async () => {
-        set(loadingState, true);
-        try {
-          const storage = await snapshot.getPromise(storageState);
-
-          storeStorage(storage!, () => true);
-          enqueueSnackbar('設定を保存しました', {
-            variant: 'success',
-            action: (
-              <Button color='inherit' size='small' variant='outlined' onClick={onBackButtonClick}>
-                プラグイン一覧に戻る
-              </Button>
-            ),
-          });
-        } finally {
-          set(loadingState, false);
-        }
-      },
-    []
-  );
-
-  return <StyledComponent {...{ onSaveButtonClick, onBackButtonClick }} />;
-};
-
-export default Container;
+export default StyledComponent;
