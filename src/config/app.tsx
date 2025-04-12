d64a10e44ba1d12c98bc143e797c39152ee28c3d
Ribbit <echo07200503@gmail.com>
2025-04-12 21:31:27 +0900

    ✨ PluginErrorBoundaryのインポートパスを修正し、RecoilRootを削除してコードを簡素化

--- src/config/app.tsx
+++ src/config/app.tsx
@@ -1,33 +1,30 @@
-import { PluginErrorBoundary } from '@/common/components/functional/error-boundary';
 import { URL_BANNER, URL_PROMOTION } from '@/common/static';
+import { PluginErrorBoundary } from '@/components/error-boundary';
 import { PluginBanner, PluginContent, PluginLayout } from '@konomi-app/kintone-utilities-react';
 import { LoaderWithLabel } from '@konomi-app/ui-react';
 import { SnackbarProvider } from 'notistack';
 import React, { Suspense } from 'react';
-import { RecoilRoot } from 'recoil';
 import Footer from './components/model/footer';
 import Form from './components/model/form';
 
 export default function App() {
   return (
     <Suspense fallback={<LoaderWithLabel label='画面の描画を待機しています' />}>
-      <RecoilRoot>
-        <PluginErrorBoundary>
-          <SnackbarProvider maxSnack={1}>
-            <Suspense fallback={<LoaderWithLabel label='設定情報を取得しています' />}>
-              <PluginLayout singleCondition>
-                <PluginContent>
-                  <PluginErrorBoundary>
-                    <Form />
-                  </PluginErrorBoundary>
-                </PluginContent>
-                <PluginBanner url={URL_BANNER} />
-                <Footer />
-              </PluginLayout>
-            </Suspense>
-          </SnackbarProvider>
-        </PluginErrorBoundary>
-      </RecoilRoot>
+      <PluginErrorBoundary>
+        <SnackbarProvider maxSnack={1}>
+          <Suspense fallback={<LoaderWithLabel label='設定情報を取得しています' />}>
+            <PluginLayout singleCondition>
+              <PluginContent>
+                <PluginErrorBoundary>
+                  <Form />
+                </PluginErrorBoundary>
+              </PluginContent>
+              <PluginBanner url={URL_BANNER} />
+              <Footer />
+            </PluginLayout>
+          </Suspense>
+        </SnackbarProvider>
+      </PluginErrorBoundary>
       <iframe
         title='promotion'
         loading='lazy'
