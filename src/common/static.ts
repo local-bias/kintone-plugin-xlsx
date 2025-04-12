d6ba71681c27dd12c1254cb4015c4b0b6b6f730c
Ribbit <echo07200503@gmail.com>
2025-04-12 21:31:17 +0900

    🔗 URL_INQUIRYを更新し、正しいお問い合わせリンクに修正

--- src/common/static.ts
+++ src/common/static.ts
@@ -9,6 +9,6 @@ export const PLUGIN_NAME = baseManifest.name.ja;
 export const LOCAL_STORAGE_KEY = 'ribbit-kintone-plugin';
 
 export const URL_HOMEPAGE = baseManifest.homepage_url.ja;
-export const URL_INQUIRY = 'https://form.konomi.app';
+export const URL_INQUIRY = 'https://konomi.app/contact';
 export const URL_PROMOTION = 'https://promotion.konomi.app/kintone-plugin';
 export const URL_BANNER = 'https://promotion.konomi.app/kintone-plugin/sidebar';
