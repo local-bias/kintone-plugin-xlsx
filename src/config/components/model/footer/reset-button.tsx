074b7113e660d04c29fe38699a99d916140513a1
Ribbit <echo07200503@gmail.com>
2025-04-12 21:31:59 +0900

    ♻ recoil → jotai

--- src/config/components/model/footer/reset-button.tsx
+++ src/config/components/model/footer/reset-button.tsx
@@ -1,4 +1,5 @@
-import React, { FC, memo, useCallback, useState } from 'react';
+import { createConfig } from '@/common/plugin';
+import RestartAltIcon from '@mui/icons-material/RestartAlt';
 import {
   Button,
   Dialog,
@@ -9,59 +10,68 @@ import {
   IconButton,
   Tooltip,
 } from '@mui/material';
-import RestartAltIcon from '@mui/icons-material/RestartAlt';
-import { useRecoilCallback } from 'recoil';
-import { storageState } from '../../../states/plugin';
-import { createConfig } from '@/common/plugin';
-import { useSnackbar } from 'notistack';
+import { atom, useAtomValue, useSetAtom } from 'jotai';
+import { enqueueSnackbar } from 'notistack';
+import React, { FC } from 'react';
+import { pluginConfigAtom } from '../../../states/plugin';
 
-const Component: FC = () => {
-  const { enqueueSnackbar } = useSnackbar();
-  const [open, setOpen] = useState<boolean>(false);
+const openAtom = atom(false);
 
-  const onIconButtonClick = useCallback(() => {
-    setOpen(true);
-  }, []);
+const handleIconButtonClickAtom = atom(null, (_, set) => {
+  set(openAtom, true);
+});
 
-  const onDecisionButtonClick = useRecoilCallback(
-    ({ set }) =>
-      () => {
-        set(storageState, createConfig());
-        setOpen(false);
-        enqueueSnackbar('設定をリセットしました', { variant: 'success' });
-      },
-    []
-  );
+const handleDialogCloseAtom = atom(null, (_, set) => {
+  set(openAtom, false);
+});
 
-  const onClose = useCallback(() => {
-    setOpen(false);
-  }, []);
+const handlePluginConfigResetAtom = atom(null, (_, set) => {
+  set(pluginConfigAtom, createConfig());
+  set(openAtom, false);
+  enqueueSnackbar('設定をリセットしました', { variant: 'success' });
+});
+
+function ResetDialog() {
+  const open = useAtomValue(openAtom);
+  const onDecisionButtonClick = useSetAtom(handlePluginConfigResetAtom);
+  const onClose = useSetAtom(handleDialogCloseAtom);
 
   return (
-    <>
-      <Dialog open={open} onClose={onClose}>
-        <DialogTitle>設定のリセット</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            このプラグインの設定を初期状態に戻します。よろしいですか？
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button color='primary' variant='contained' onClick={onDecisionButtonClick}>
-            実行
-          </Button>
-          <Button color='inherit' variant='contained' onClick={onClose}>
-            キャンセル
-          </Button>
-        </DialogActions>
-      </Dialog>
-      <Tooltip title='プラグインの設定をリセット'>
-        <IconButton onClick={onIconButtonClick}>
-          <RestartAltIcon />
-        </IconButton>
-      </Tooltip>
-    </>
+    <Dialog open={open} onClose={onClose}>
+      <DialogTitle>設定のリセット</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          このプラグインの設定を初期状態に戻します。よろしいですか？
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button color='primary' variant='contained' onClick={onDecisionButtonClick}>
+          実行
+        </Button>
+        <Button color='inherit' variant='contained' onClick={onClose}>
+          キャンセル
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
+
+const ResetButtonComponent: FC = () => {
+  const onIconButtonClick = useSetAtom(handleIconButtonClickAtom);
+  return (
+    <Tooltip title='プラグインの設定をリセット'>
+      <IconButton onClick={onIconButtonClick}>
+        <RestartAltIcon />
+      </IconButton>
+    </Tooltip>
   );
 };
 
-export default memo(Component);
+export default function ResetButton() {
+  return (
+    <>
+      <ResetDialog />
+      <ResetButtonComponent />
+    </>
+  );
+}
